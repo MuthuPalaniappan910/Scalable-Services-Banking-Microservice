3781863e35ba0a1305bf438bc23a58d7df9b89ac
tpannerselvam <thamaraikanni.pannerselvam@lastminute.com>
2023-04-26 19:14:11 +0530

    By mistake category is renamed as account. Reverted the change.

--- product/product.sql
+++ product/product.sql
@@ -1,6 +1,6 @@
 use product;
 
-CREATE TABLE account
+CREATE TABLE category
 (
     Category_Id          bigint auto_increment PRIMARY KEY,
     Category VARCHAR(100) NULL
@@ -12,13 +12,13 @@ CREATE TABLE product
     Product VARCHAR(100) NULL,
     Category_Id bigint NOT NULL,
     FOREIGN KEY (Category_Id)
-        REFERENCES account(Category_Id)
+        REFERENCES category(Category_Id)
 );
 
 
-Insert into account values (1,'Savings');
-Insert into account values (2,'Credit');
-Insert into account values (3,'Loan');
+Insert into category values (1,'Savings');
+Insert into category values (2,'Credit');
+Insert into category values (3,'Loan');
 
 Insert into product values(1,'SA1',1);
 Insert into product values(2,'SA2',1);
